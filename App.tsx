ef883eef638e550dfd9a8ef2ac4e8d295e1f4f7a
Nizam <ramazanov570633@gmail.com>
2023-09-19 11:24:40 +0400

    End1

--- App.tsx
+++ App.tsx
@@ -23,7 +23,7 @@ const App = () => {
         <Stack.Screen name="SplasScreen" component={SplasScreen} />   
       <Stack.Screen name="OnboardinScreen" component={OnboardinScreen} /> 
       <Stack.Screen name="LoginScreen" component={LoginScreen} />   
-      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />  
+      {/* <Stack.Screen name="RegisterScreen" component={RegisterScreen} />   */}
         <Stack.Screen name="Tabbar" component={Tabbar} />
         <Stack.Screen name="DoctorScreen" component={DoctorScreen} />
         <Stack.Screen name="DermanScreen" component={DermanScreen} />
