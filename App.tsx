a051698a15ee5c5f25331458f1db6e288e4adb67
Nizam <ramazanov570633@gmail.com>
2023-10-01 20:38:00 +0400

    Fix21

--- App.tsx
+++ App.tsx
@@ -20,11 +20,11 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="SplasScreen" component={SplasScreen} />   
+        {/* <Stack.Screen name="SplasScreen" component={SplasScreen} />   
       <Stack.Screen name="OnboardinScreen" component={OnboardinScreen} /> 
       <Stack.Screen name="LoginScreen" component={LoginScreen} />   
        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />  
-        <Stack.Screen name="Tabbar" component={Tabbar} />
+        <Stack.Screen name="Tabbar" component={Tabbar} /> */}
         <Stack.Screen name="DoctorScreen" component={DoctorScreen} />
         <Stack.Screen name="DermanScreen" component={DermanScreen} />
       </Stack.Navigator>
